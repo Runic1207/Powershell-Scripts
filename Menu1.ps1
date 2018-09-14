09a1198b3e9b2d6d84fc80aef6e59a5a0edead90
3ountyhunter <35637752+3ountyhunter@users.noreply.github.com>
2018-09-14 02:59:43 -0400

    v4
    Added more commands to fulfill menu items

--- Menu1.ps1
+++ Menu1.ps1
@@ -6,19 +6,19 @@ function Show-Menu
      cls
      Write-Host "================ $Title ================"
 
-     Write-Host "1: Press '1' for Hostname."
-     Write-Host "2: Press '2' to show IP Address." -BackgroundColor Black
-     Write-Host "3: Press '3' to show DNS Servers." -BackgroundColor DarkMagenta
-     Write-Host "4: Press '4' to show Default Gateway." -BackgroundColor Black
-     Write-Host "5: Press '5' to show Available Memory." -BackgroundColor DarkMagenta
-     Write-Host "6: Press '6' to show Path." -BackgroundColor Black
-     Write-Host "7: Press '7' to list installed drivers." -BackgroundColor DarkMagenta
-     Write-Host "8: Press '8' to show running tasks." -BackgroundColor Black
-     Write-Host "9: Press '9' to kill task." -BackgroundColor DarkMagenta
+     Write-Host "1:  Press '1' for Hostname."
+     Write-Host "2:  Press '2' to show IP Address." -BackgroundColor Black
+     Write-Host "3:  Press '3' to show DNS Servers." -BackgroundColor DarkMagenta
+     Write-Host "4:  Press '4' to show Default Gateway." -BackgroundColor Black
+     Write-Host "5:  Press '5' to show Available Memory." -BackgroundColor DarkMagenta
+     Write-Host "6:  Press '6' to show Path." -BackgroundColor Black
+     Write-Host "7:  Press '7' to list installed drivers." -BackgroundColor DarkMagenta
+     Write-Host "8:  Press '8' to show running tasks." -BackgroundColor Black
+     Write-Host "9:  Press '9' to kill task." -BackgroundColor DarkMagenta
      Write-Host "10: Press '10' to display file." -BackgroundColor Black
      Write-Host "11: Press '11' for file security information." -BackgroundColor DarkMagenta
      Write-Host "12: Press '12' to show current network connections and listening ports." -BackgroundColor Black
-     Write-Host "Q: Press 'Q' to quit." -BackgroundColor DarkMagenta
+     Write-Host "Q:  Press 'Q' to quit." -BackgroundColor DarkMagenta
 
 }
 do
@@ -41,15 +41,16 @@ do
            } '3' {
     clear-host
     "------------DNS SERVER ADDRESSES------------"
-    Get-NetIPConfiguration | Get-DnsClientServerAddress
+    (Get-NetIPConfiguration | Get-DnsClientServerAddress)
 
           } '4' {
     clear-host
     "--------------DEFAULT GATEWAY---------------"
-    Get-NetIPConfiguration | Foreach IPv4DefaultGateway
+    (Get-NetIPConfiguration | Foreach IPv4DefaultGateway)
 
           } '5' {
     clear-host
+    $freemem = Get-WmiObject -Class Win32_OperatingSystem
     "----------------FREE MEMORY-----------------"
     ""
     "System Name     : {0}" -f $freemem.csname
@@ -65,23 +66,32 @@ do
           } '7' {
     clear-host
     "-------------INSTALLED DRIVERS--------------"
+    (Get-WindowsDriver -online -all)
 
           } '8' {
     clear-host
     "---------------RUNNING TASKS----------------"
-
+    Get-Process
 
           } '9' {
     clear-host
     "------------ENTER PROCESS TO KILL-----------"
+    Get-Process
+    $task = Read-Host "Enter PID of Process to Kill"
+    (Stop-Process $task)
 
           } '10' {
     clear-host
       "---------------DISPLAY FILE---------------"
-
+      ""
+      $file = Read-Host "ENTER PATH OF FILE TO DISPLAY"
+      (Get-Content $file)
           } '11' {
     clear-host
     "---------FILE SECURITY INFORMATION----------"
+    ""
+    $security = Read-Host "ENTER PATH OF FILE: "
+    (Get-Acl $security)
 
           } '12' {
     clear-host
