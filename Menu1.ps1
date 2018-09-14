486a59e831e2def5176eb307e83f9e92249ac485
3ountyhunter <35637752+3ountyhunter@users.noreply.github.com>
2018-09-14 03:24:53 -0400

    v4.2
    Changed background to a variable to allow changing of font color across all alternating rows. Replaced aliased cmdlet with proper verison.

--- Menu1.ps1
+++ Menu1.ps1
@@ -1,26 +1,32 @@
 # This script was written by Michael Heeney and ***** ***** for Scripting for Network Administrators for Baker College of Jackson.
 # First build date: 9/14/18
+#--------------------------------------------------------------------------------------------------------------------------------
+
+#Sets background and font color of text
+$backgroundColor = "DarkMagenta"
+$host.UI.RawUI.ForegroundColor = "White"
+$host.UI.RawUI.BackgroundColor = "$backgroundColor"
 function Show-Menu
 {
      param (
            [string]$Title = 'Choose an option:'
      )
-     cls
+     Clear-Host
      Write-Host "================ $Title ================"
 
-     Write-Host "1:  Press '1' for Hostname."
+     Write-Host "1:  Press '1' for Hostname." -BackgroundColor $backgroundColor
      Write-Host "2:  Press '2' to show IP Address." -BackgroundColor Black
-     Write-Host "3:  Press '3' to show DNS Servers." -BackgroundColor DarkMagenta
+     Write-Host "3:  Press '3' to show DNS Servers." -BackgroundColor $backgroundColor
      Write-Host "4:  Press '4' to show Default Gateway." -BackgroundColor Black
-     Write-Host "5:  Press '5' to show Available Memory." -BackgroundColor DarkMagenta
+     Write-Host "5:  Press '5' to show Available Memory." -BackgroundColor $backgroundColor
      Write-Host "6:  Press '6' to show Path." -BackgroundColor Black
-     Write-Host "7:  Press '7' to list installed drivers." -BackgroundColor DarkMagenta
+     Write-Host "7:  Press '7' to list installed drivers." -BackgroundColor $backgroundColor
      Write-Host "8:  Press '8' to show running tasks." -BackgroundColor Black
-     Write-Host "9:  Press '9' to kill task." -BackgroundColor DarkMagenta
+     Write-Host "9:  Press '9' to kill task." -BackgroundColor $backgroundColor
      Write-Host "10: Press '10' to display file." -BackgroundColor Black
-     Write-Host "11: Press '11' for file security information." -BackgroundColor DarkMagenta
+     Write-Host "11: Press '11' for file security information." -BackgroundColor $backgroundColor
      Write-Host "12: Press '12' to show current network connections and listening ports." -BackgroundColor Black
-     Write-Host "Q:  Press 'Q' to quit." -BackgroundColor DarkMagenta
+     Write-Host "Q:  Press 'Q' to quit." -BackgroundColor $backgroundColor
 
 }
 do
